73758e22460e5c442f3fdf4a4a7a86bb4a9f6c96
evaristocuesta <evaristocg@gmail.com>
2022-05-14 15:05:09 +0200

    Add entity framework test to console app

--- CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
@@ -5,10 +5,36 @@ StreamerDbContext dbContext = new();
 
 Streamer streamer = new()
 {
-    Name = "Netflix",
-    Url = "https://www.netflix.com"
+    Name = "Amazon Prime",
+    Url = "https://www.amazonprime.com"
 };
 
-dbContext!.Streamers!.Add(streamer);
+dbContext.Streamers!.Add(streamer);
 await dbContext!.SaveChangesAsync();
 
+var movies = new List<Movie>
+{
+    new Movie
+    {
+        Title = "The Goonies",
+        StreamerId = streamer.Id
+    },
+    new Movie
+    {
+        Title = "Ghostbusters",
+        StreamerId = streamer.Id
+    },
+   new Movie
+    {
+        Title = "Citizen Kane",
+        StreamerId = streamer.Id
+    },
+    new Movie
+    {
+        Title = "Indiana Jones Raiders of the Lost Ark",
+        StreamerId = streamer.Id
+    },
+};
+
+await dbContext.Movies!.AddRangeAsync(movies);
+await dbContext.SaveChangesAsync();
