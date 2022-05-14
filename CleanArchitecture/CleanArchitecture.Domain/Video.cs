90313918b1249b4900bdbea0136f2a2ce859b629
evaristocuesta <evaristocg@gmail.com>
2022-05-14 14:29:49 +0200

    Add project files.

---
+++ CleanArchitecture/CleanArchitecture.Domain/Video.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanArchitecture.Domain
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public int StreamerId { get; set; }
+
+        public virtual Streamer? Streamer { get; set; }
+    }
+}
