c5c508818434491289fa7e49543d47919a4f2c7b
evaristocuesta <evaristocg@gmail.com>
2022-05-15 13:30:25 +0200

    Add email service

---
+++ src/Infrastructure/CleanArchitecture.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Models;
+using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
+
+namespace CleanArchitecture.Infrastructure.Email
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _emailSettings;
+        private readonly ILogger<EmailService> _logger;
+
+        public EmailService(EmailSettings emailSettings, ILogger<EmailService> logger)
+        {
+            _emailSettings = emailSettings;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendEmail(Application.Models.Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+            var to = new EmailAddress(email.To);
+
+            var from = new EmailAddress
+            {
+                Email = _emailSettings.FromAdress,
+                Name = _emailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, email.Subject, email.Body, email.Body);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            if (response.StatusCode != HttpStatusCode.Accepted && response.StatusCode != HttpStatusCode.OK)
+            {
+                _logger.LogError("Error sending email");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
