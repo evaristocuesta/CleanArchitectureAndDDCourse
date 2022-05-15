b1dff57c3dd22784cb75406e48d5a05e3f7d3766
evaristocuesta <evaristocg@gmail.com>
2022-05-15 14:42:25 +0200

    Add base domain model colums migration

---
+++ src/Infrastructure/CleanArchitecture.Infrastructure/Migrations/20220515123154_AddBaseDomainModelColumns.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Infrastructure.Migrations
+{
+    public partial class AddBaseDomainModelColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Streamers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Streamers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                table: "Streamers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedDate",
+                table: "Streamers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Movies",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedDate",
+                table: "Movies",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Director",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Director",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                table: "Director",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedDate",
+                table: "Director",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Actor",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Actor",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                table: "Actor",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedDate",
+                table: "Actor",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Streamers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Streamers");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "Streamers");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedDate",
+                table: "Streamers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedDate",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Director");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Director");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "Director");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedDate",
+                table: "Director");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Actor");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Actor");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "Actor");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedDate",
+                table: "Actor");
+        }
+    }
+}
