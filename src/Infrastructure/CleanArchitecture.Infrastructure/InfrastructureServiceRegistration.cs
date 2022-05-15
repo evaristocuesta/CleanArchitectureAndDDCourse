beddd10b92a8a8159c9f772aecee6094d24b3208
evaristocuesta <evaristocg@gmail.com>
2022-05-15 13:41:22 +0200

    Add infrastructure service registration

---
+++ src/Infrastructure/CleanArchitecture.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Infrastructure.Email;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<StreamerDbContext>(option =>
+                option.UseSqlServer(configuration.GetConnectionString("ConnectionString"))
+            );
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddScoped<IStreamerRepository, StreamerRepository>();
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
