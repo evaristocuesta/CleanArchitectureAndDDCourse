68d91931e85bde997ffd956c419390fe7337ad5d
evaristocuesta <evaristocg@gmail.com>
2022-05-15 11:22:55 +0200

    Add streamer db context seed

---
+++ src/Infrastructure/CleanArchitecture.Infrastructure/Persistence/StreamerDbContext.cs
@@ -0,0 +1,54 @@
+﻿using CleanArchitecture.Domain;
+using CleanArchitecture.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public class StreamerDbContext : DbContext
+    {
+        private const string SYSTEM_USER = "system";
+
+        public StreamerDbContext(DbContextOptions<StreamerDbContext> options) : base(options)
+        {
+
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.UtcNow;
+                        entry.Entity.CreatedBy = "system";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.UtcNow;
+                        entry.Entity.LastModifiedBy = SYSTEM_USER;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Streamer;Integrated Security=True")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>()
+                .HasMany(m => m.Actors)
+                .WithMany(a => a.Movies)
+                .UsingEntity<MovieActor>(ma => ma.HasKey(e => new { e.ActorId, e.MovieId }));
+        }
+
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Movie>? Movies { get; set; }
+    }
+}
---
+++ src/Infrastructure/CleanArchitecture.Infrastructure/Persistence/StreamerDbContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public class StreamerDbContextSeed
+    {
+        private const string SEED_USER = "seed";
+
+        public static async Task SeedAsync(StreamerDbContext context, ILogger<StreamerDbContextSeed> logger)
+        {
+            if (!context.Streamers!.Any())
+            {
+                await context.Streamers!.AddRangeAsync(GetPreconfiguredStreamer());
+                await context.SaveChangesAsync();
+                logger.LogInformation("Seeding streamers to {db}", typeof(StreamerDbContext).Name);
+            }
+        }
+
+        private static IEnumerable<Streamer> GetPreconfiguredStreamer()
+        {
+            return new List<Streamer>
+            {
+                new Streamer { CreatedBy = SEED_USER, Name = "Netflix", Url = "https://www.netflix.com" },
+                new Streamer { CreatedBy = SEED_USER, Name = "Amazon Prime", Url = "https://www.amazonprime.com" },
+                new Streamer { CreatedBy = SEED_USER, Name = "HBO Max", Url = "https://www.hbomax.com" }
+            };
+        }
+    }
+}
