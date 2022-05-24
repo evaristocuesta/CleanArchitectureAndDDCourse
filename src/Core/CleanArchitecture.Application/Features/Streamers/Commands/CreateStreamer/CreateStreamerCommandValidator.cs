1c1448e4d2945bc733fe032a1a89cdbb1dc84c91
evaristocuesta <evaristocg@gmail.com>
2022-05-24 19:22:02 +0200

    Update Streamer validation

--- src/Core/CleanArchitecture.Application/Features/Streamers/Commands/CreateStreamer/CreateStreamerCommandValidator.cs
+++ src/Core/CleanArchitecture.Application/Features/Streamers/Commands/CreateStreamer/CreateStreamerCommandValidator.cs
@@ -9,7 +9,8 @@ namespace CleanArchitecture.Application.Features.Streamers.Commands.CreateStream
             RuleFor(s => s.Name)
                 .NotEmpty().WithMessage("Name is required")
                 .NotNull().WithMessage("Name is required")
-                .MaximumLength(50).WithMessage("Name lenght must be less than 50");
+                .MaximumLength(50).WithMessage("Name lenght must be less than 50")
+                .MinimumLength(6).WithMessage("Name lenght must be more than 6");
 
             RuleFor(s => s.Url)
                 .NotEmpty().WithMessage("Url is required");
